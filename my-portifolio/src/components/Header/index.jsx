43fc8c697941a1af3897280fb4ba334be64c3ee1
Jefferson souza <582jeffersons@gmail.com>
2023-12-28 12:15:32 +0000

    Responsividade para dispositivos moveis

--- my-portifolio/src/components/Header/index.jsx
+++ my-portifolio/src/components/Header/index.jsx
@@ -1,20 +1,34 @@
-import styles from './Header.module.css'
-import {Link} from 'react-router-dom'
+import { useState } from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        <header className={styles.header}>
-                <Link to="/">
-            <span>Dev.jefferson</span>
-                </Link>
-            <nav>
-                 <Link to="/">Home</Link>
-                 <Link to="/Sobre">Sobre</Link>
-                 <Link to="/Projetos">Projetos</Link>
-                 <Link to="/Contatos">Contatos</Link>
-            </nav>
-        </header>
-    )
+  const [showMenu, setShowMenu] = useState(false);
+  const togglemenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <header className={styles.header}>
+      <Link to="/">
+        <span>Dev.jefferson</span>
+      </Link>
+      <nav
+        className={`${styles.menuSandwich} ${showMenu ? styles.show : ""}`}
+        onClick={togglemenu}
+      >
+        <Link to="/">Home</Link>
+        <Link to="/Sobre">Sobre</Link>
+        <Link to="/Projetos">Projetos</Link>
+        <Link to="/Contatos">Contatos</Link>
+      </nav>
+      <div className={styles.menuButton} onClick={togglemenu}>
+        <span className={styles.linha}></span>
+        <span className={styles.linha}></span>
+        <span className={styles.linha}></span>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- my-portifolio/src/pages/Pages404/index.jsx
+++ my-portifolio/src/pages/Pages404/index.jsx
@@ -2,13 +2,13 @@ import styles from './Page404.module.css'
 
 function Page404 (){
     return(
-        <>         
-            <h2 className={styles.titulo2}>Ops, Algo deu errado !</h2>
-            <div className={styles.textos}>
-                <span className={styles.texto_grande}>404</span> <br />
-                <strong className={styles.texto_vermelho}>Página Não Localizada !</strong>
-            </div>   
-        </>
+        <section className={styles.page404}>         
+                <h2 className={styles.titulo2}>Ops, Algo deu errado !</h2>
+                <div className={styles.textos}>
+                    <span className={styles.texto_grande}>404</span> <br />
+                    <strong className={styles.texto_vermelho}>Página Não Localizada !</strong>
+                </div>   
+        </section>
 
     )
 }
