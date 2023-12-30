e2caa6c98e8502b8a794d0dd02e2c07fc2659842
Jefferson souza <582jeffersons@gmail.com>
2023-12-30 15:01:48 +0000

    Implementando a foto na pessoal na pagina sobre.

--- my-portifolio/src/pages/Sobre/index.jsx
+++ my-portifolio/src/pages/Sobre/index.jsx
@@ -1,17 +1,17 @@
 import styles from "./Sobre.module.css";
-import avatar from "./images/avatar.svg";
 import html from './images/icon-html.svg'
 import css from './images/icon-css.svg'
 import js from './images/icon-js.svg'
 import react from './images/icon-react.svg'
 import sql from './images/icon-sql.svg'
+import logo from './images/fotopessoal.jpg'
 
 function Sobre() {
   return (
     <>
       <section className={styles.sobre}>
         <div className={styles.bio}>
-          <img src={avatar} alt="Avatar do usuario" className={styles.avatar} />
+          <img src={logo} alt="Avatar do usuario" className={styles.avatar} />
 
           <div className={styles.texto}>
             <h2>Sobre</h2>
